49ad12b340d92b51ae0e79f6926b9639531f0352
Ramesh Rathod <rameshl.rathod065@gmail.com>
2022-09-15 10:21:27 +0530

    Handling CRUD with API(GET,POST,PUT, and DELETE) in mongoose

---
+++ config.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+const dburl = 'mongodb://localhost:27017/youtube';
+mongoose.connect(dburl);
--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-/* 
-const http = require('http');
-const data = require('./data');
-http.createServer((req,resp)=>{
-    resp.writeHead(200, {'content-Type':'application/json'});
-    resp.write(JSON.stringify(data));
-    resp.end();
-}).listen(5000)
+const express = require('express');
+require("./config");
+const Product = require('./product');
+const Video = require('./video');
 
- */
-const fs = require('fs');
+const app = express();
+app.use(express.json());
+app.post('/create',async (req,resp)=>{
+    let data = new Product(req.body);
+    const result = await data.save();
+    resp.send(result);
+});
 
-const path = require('path');
-const dirPath = path.join(__dirname,'files');
-console.log(dirPath);
-
-/* for(i=0;i<5;i++){
-    fs.writeFileSync(`${dirPath}/Test${i}.txt`,`This is just a  test file`)
-} */
-
-fs.readdir(dirPath,(err,files)=>{
-    files.map(item=>{
-        console.log(`This file is ${item}`);
-    });
-    files.forEach(item=>{
-        console.log(`file name is ${item}`);
-    })
+app.get("/list", async (req, resp) => {
+    let data = await Product.find();
+    resp.send(data);
 })
 
+app.delete("/delete/:_id", async (req, resp) => {
+    console.log(req.params)
+    let data = await Product.deleteOne(req.params);
+    resp.send(data);
+})
+
+
+app.put("/update/:_id",async (req, resp) => {
+    console.log(req.params)
+    let data = await Product.updateOne(
+        req.params,
+        {$set: req.body}
+    );
+    resp.send(data);
+})
+
+app.listen(5000);
