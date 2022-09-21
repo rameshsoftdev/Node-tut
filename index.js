b2a6d8e9724c51813563f23b334a0a41cfdc6beb
Ramesh Rathod <rameshl.rathod065@gmail.com>
2022-09-21 10:17:58 +0530

    OS Module and Event Emmiter

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require('express');
-require("./config");
-const Product = require('./product');
-
+const EventEmitter = require('events');
+const event = new EventEmitter();
 const app = express();
-app.use(express.json());
-
-app.get('/search/:key',async (req,resp)=>{
-     let data = await Product.find({
-        '$or':[
-            {"name":{$regex:req.params.key}},
-            {"category":{$regex:req.params.key}},
-            {"brand":{$regex:req.params.key}}
-        ]
-     });
-     resp.send(data);
+/* 
+const os = require('os');
+console.log(os.arch());
+console.log(os.freemem()/(1024*1024*1024));
+console.log(os.totalmem()/(1024*1024*1024));
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.userInfo());
+*/
+let count = 0;
+event.on('eventAPI',()=>{
+    count++;
+    console.log('count',count);
 })
 
+app.get('/',(req,resp)=>{
+   resp.send('root');
+   event.emit('eventAPI');
+});
+
+app.get('/test',(req,resp)=>{
+    resp.send('test');
+    event.emit('eventAPI');
+ });
 
 app.listen(5000);
